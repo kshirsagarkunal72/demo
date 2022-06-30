4317d49653bfeff4b997cf64bd3813813c443aef
kshirsagarkunal72 <kshirsagarkunal72@gmail.com>
2022-06-30 11:20:37 +0530

    30

--- CalculatorProgram.java
+++ CalculatorProgram.java
@@ -9,11 +9,11 @@ public class CalculatorProgram {
 		int result;
 
 	    // create an object of Scanner class
-	    Scanner s = new Scanner(System.in);
+//	    Scanner s = new Scanner(System.in);
 
 	    // ask users to enter operator
-	    System.out.println("Choose an operator: 1.add, 2.sub, 3.multiply, or 4.divide");
-	    c = s.nextInt();
+	    System.out.println("Actions: 1.add, 2.sub, 3.multiply, or 4.divide");
+//	    c = s.nextInt();
 
 	    // ask users to enter numbers
 	    /*System.out.println("Enter first number");
@@ -25,37 +25,30 @@ public class CalculatorProgram {
 	    number1=12;
 	    number2=8;
 
-	    switch (c) {
-
 	      // performs addition between numbers
-	      case 1:
 	        result = number1 + number2;
 	        System.out.println(number1 + " + " + number2 + " = " + result);
-	        break;
 
 	      // performs subtraction between numbers
-	      case 2:
 	        result = number1 - number2;
 	        System.out.println(number1 + " - " + number2 + " = " + result);
-	        break;
+	   
 
 	      // performs multiplication between numbers
-	      case 3:
+	
 	        result = number1 * number2;
 	        System.out.println(number1 + " * " + number2 + " = " + result);
-	        break;
+	        
 
 	      // performs division between numbers
-	      case 4:
+	      
 	        result = number1 / number2;
 	        System.out.println(number1 + " / " + number2 + " = " + result);
-	        break;
+	       
 
-	      default:
-	        System.out.println("Invalid operator!");
-	        break;
+	      
+	        System.out.println("Done sucesfully!");
+	      
 	    }
 
-	    s.close();
 	  }
-	}
