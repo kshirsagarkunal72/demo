879dbb191681fb96f59b4e66a01458926ba5b11e
kshirsagarkunal72 <kshirsagarkunal72@gmail.com>
2022-07-04 11:13:45 +0530

    Added java demo application files

---
+++ OddEven.java
@@ -0,0 +1,13 @@
+package com.app;
+
+public class OddEven {
+	
+	public static void main(String[] args) {
+		System.out.println(checkIfInputIsAnEvenNumber(122));
+		// Testing in the main method
+	}
+
+	public static boolean checkIfInputIsAnEvenNumber(int number) {
+		return number % 2 == 0;
+	}
+}
---
+++ TestMain.java
@@ -0,0 +1,11 @@
+package com.app;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestMain {
+@Test
+public void testInputIsEven(){
+assertTrue(OddEven.checkIfInputIsAnEvenNumber(23)); //Assertion
+}
+}
